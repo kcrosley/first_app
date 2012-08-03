e235c985b799f0656d36eefac3823ddf1a670f2b
Keith R. Crosley <heroku@submitto.us>
2012-08-03 13:08:23 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '86de99e55190e12ec57ebb16da9b025dd278dff8bd031f54b323e5f138c78bd27e52306fd855783c0c86894e37b3185958cff26a2e526f515c9f1f277a2a0049'
